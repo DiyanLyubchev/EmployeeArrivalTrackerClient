143d9649f9b047f5f92e3b435dea13f881c8e499
DiyanLyubchev <Diyan.Lyubchev@gmail.com>
2023-02-11 12:00:47 +0200

    Add custom validations

--- EmployeeArrivalTrackerClient/EmployeeArrivalTrackerDomain/Validators/ProducerArrivalEmployeesVMValidator.cs
+++ EmployeeArrivalTrackerClient/EmployeeArrivalTrackerDomain/Validators/ProducerArrivalEmployeesVMValidator.cs
@@ -1,5 +1,6 @@
 ﻿using Common.Models.Producer;
 using FluentValidation;
+using System;
 
 namespace EmployeeArrivalTrackerDomain.Validators
 {
@@ -8,7 +9,24 @@ namespace EmployeeArrivalTrackerDomain.Validators
         public ProducerArrivalEmployeesVMValidator()
         {
             RuleFor(p => p.EmployeeId).NotEmpty();
+
             RuleFor(p => p.When).NotEmpty();
+
+            RuleFor(u => u.When)
+                    .Cascade(CascadeMode.Continue)
+                    .Custom(ValidateDate);
+        }
+
+        private void ValidateDate(DateTime date, ValidationContext<ProducerArrivalEmployeesVM> context)
+        {
+            try
+            {
+                DateTime.SpecifyKind(date, DateTimeKind.Utc);
+            }
+            catch (Exception ex)
+            {
+                context.AddFailure(ex.Message);
+            }
         }
     }
 }
